d77e43bfbb90a393020bc1599578ae4631a4fdc8
Achyut Neupane <achyutkneupane@gmail.com>
2023-12-30 02:55:23 +0545

    test: Factories added

--- src/Factories/CommentFactory.php
+++ src/Factories/CommentFactory.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CommentFactory extends Factory
 {
-
     /**
      * The name of the factory's corresponding model.
      *
--- tests/Models/User.php
+++ tests/Models/User.php
@@ -2,11 +2,21 @@
 
 namespace AchyutN\LaravelComment\Tests\Models;
 
+use AchyutN\LaravelComment\Tests\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Model;
 use AchyutN\LaravelComment\Traits\CanComment;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class User extends Model
 {
     use CanComment;
     protected $guarded = [];
+    protected static function factory(int $count = 1): Factory
+    {
+        if($count && $count > 1) {
+            return UserFactory::times($count);
+        } else {
+            return UserFactory::new();
+        }
+    }
 }
