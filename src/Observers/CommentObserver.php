1f1436fc3ef37c0293a38b969e472abfb71e6848
Achyut Neupane <achyutkneupane@gmail.com>
2023-12-30 13:08:51 +0545

    feat: CommentObserver to handle pending factory entry

--- src/Observers/CommentObserver.php
+++ src/Observers/CommentObserver.php
@@ -7,11 +7,22 @@ use AchyutN\LaravelComment\Models\Comment;
 class CommentObserver
 {
     /**
-     * Handle the Comment "created" event.
+     * Handle the Comment "creating" event.
      */
-    public function created(Comment $comment): void
+    public function creating(Comment $comment): void
     {
+        /**
+         * If the comment is pending, set approved_at to null by default
+         */
+        if($comment->pending) {
+            unset($comment->pending);
+            return;
+        }
+
         $manualApproval = config('comment.manual_approval');
+        if ($manualApproval === null) {
+            $manualApproval = false;
+        }
 
         if (!$manualApproval) {
             $comment->update([
