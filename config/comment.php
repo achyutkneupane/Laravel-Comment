fda5781acca02b122a76499e082e6ef873ba0e3a
Achyut Neupane <achyutkneupane@gmail.com>
2023-12-30 13:25:57 +0545

    feat: Manual Approval Option added in config file

--- config/comment.php
+++ config/comment.php
@@ -14,4 +14,12 @@ return [
      * By default, the default Laravel User model is used.
      */
     'commenter_model' => \App\Models\User::class,
+
+    /**
+     * Manual approval of comments.
+     *
+     * By default, the comments are approved automatically.
+     * You can change this to false if you want to approve comments manually.
+     */
+    'manual_approval' => false,
 ];
